6a8092b4844e0d25de218fb1265a222425975003
Brian Bowen <phatblinkie@gmail.com>
2025-10-10 02:15:41 +0000

    bug fixes in flat vs not flat exports

--- podman-export-flat-cmd.sh
+++ podman-export-flat-cmd.sh
@@ -5,17 +5,17 @@ then
 	echo you must run this with selinux disabled sorry, type "sudo setenforce 0" and retry
 	exit 1
 fi
-podman run -it --rm --name aap-porter -v "$PWD":/mnt localhost/aap-porter:latest bash -c 'cd /mnt/ && ./export.cmdline.notflat.sh'
-if [ -d ../export ]
+podman run -it --rm --name aap-porter -v "$PWD":/mnt localhost/aap-porter:latest bash -c 'cd /mnt/ && ./export.cmdline.flat.sh'
+if [ -d ../flatexport ]
 then
-	rm -f ../export.backup 
-	echo "moving old export dir to export.backup"
-	mv -fv ../export ../export.backup
-	mv export ../
+	rm -f ../flatexport.backup 
+	echo "moving old flatexport dir to flatexport.backup"
+	mv -fv ../flatexport ../flatexport.backup
+	mv flatexport ../
 else
-        mv -fv export ../
+        mv -fv flatexport ../
 fi
-echo "The exported files are in export"
+echo "The exported files are in flatexport"
 echo
 echo
 echo
