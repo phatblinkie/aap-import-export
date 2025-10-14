975a53763df7dd9aaeef98ac9b803cd42462c671
Brian Bowen <phatblinkie@gmail.com>
2025-10-14 03:12:06 +0000

    fixes to import scripts, had a bad mapping.

--- podman-import-flat-cmd.sh
+++ podman-import-flat-cmd.sh
@@ -5,5 +5,5 @@ then
 	echo you must run this with selinux disabled sorry, type "sudo setenforce 0" and retry
 	exit 1
 fi
-podman run -it --rm --name aap-porter -v "$PWD":/mnt localhost/aap-porter:latest bash -c 'cd /mnt/ && ./import.cmdline.flat.sh'
+podman run -it --rm --name aap-porter -v "$PWD":/mnt -v "$PWD/../flatexport":/mnt/flatexport localhost/aap-porter:latest bash -c 'cd /mnt/ && ./import.cmdline.flat.sh'
 
